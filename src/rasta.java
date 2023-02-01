7af728fdb31673e4d68059194f676dfcafe16648
Rayane Bouchair <bouchairrayane01@gmail.com>
2023-02-01 21:16:33 +0100

    first commit

---
+++ src/rasta.java
@@ -0,0 +1,7 @@
+public class rasta{
+    public static void main(String[] args){
+        System.out.println("welcom in my first program in Java");
+        System.out.println("stay tonned <3 <-> ");
+        System.out.println("^_^ ^_^ ^_^.........to be continued");
+    }
+}
