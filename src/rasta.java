f870744907b26f4df254344b833f374eaa3d04c7
Rayane Bouchair <bouchairrayane01@gmail.com>
2023-02-01 22:26:38 +0100

    some basics by rayane

--- src/rasta.java
+++ src/rasta.java
@@ -1,7 +1,20 @@
 public class rasta{
     public static void main(String[] args){
         System.out.println("welcom in my first program in Java");
-        System.out.println("stay tonned <3 <-> ");
-        System.out.println("^_^ ^_^ ^_^.........to be continued");
+        
+        String varst ="------>My name is Rayane ,i'm a developer ,and i'm 19 years old ";
+        char var1 ='R';
+        String var2 ="--->Hello im a string var";
+        boolean bool = false;
+        int nbr =1233; 
+//other types of number : float ,double,short ,long...............
+
+
+        System.out.println(varst);         //how to print  a string variable .
+        System.out.println("-->this is the character var .");   //result in instruction after this .
+        System.out.println(var1);          //how to print a character variable .
+        System.out.println(var2);          //who to print a string variable again .
+        System.out.println(bool);          //how to print a bollean variable .
+        System.out.println(nbr);           //how to print a simple number .
     }
 }
